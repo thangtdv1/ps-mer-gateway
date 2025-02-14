05e82738ecf37a18309b4fbbe5e60e408b3d4f7f
thangtdv1 <trandinhthangv1@gmail.com>
2025-02-15 00:48:22 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  console.log('NODE_ENV', process.env.NODE_ENV);
+  console.log('PORT', process.env.PORT);
+
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 8000);
+}
+
+bootstrap();
